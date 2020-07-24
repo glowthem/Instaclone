fdd1ddd73af28a4cd530a1cb130f5af71d81c836
glowthem <glowing713@naver.com>
2020-07-24 16:42:00 +0900

    2020-07-24 👍 finished like/unlike a photo backend

--- src/api/Post/toggleLike/toggleLike.js
+++ src/api/Post/toggleLike/toggleLike.js
@@ -8,25 +8,27 @@ export default {
       isAuthenticated(request);
       const { postId } = args;
       const { user } = request;
+      const filterOptions = {
+        AND: [
+          {
+            user: {
+              id: user.id,
+            },
+          },
+          {
+            post: {
+              id: postId,
+            },
+          },
+        ],
+      };
+
       try {
-        const existingLike = await prisma.$exists.like({
-          AND: [
-            {
-              user: {
-                id: user.id,
-              },
-            },
-            {
-              post: {
-                id: postId,
-              },
-            },
-          ],
-        });
+        const existingLike = await prisma.$exists.like(filterOptions);
         if (existingLike) {
-          // TO DO
+          await prisma.deleteManyLikes(filterOptions);
         } else {
-          const newLike = await prisma.createLike({
+          await prisma.createLike({
             user: {
               connect: {
                 id: user.id,
